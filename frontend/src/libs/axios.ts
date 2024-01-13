291583afac14c72cea6fd6c8c8c4795c7fc13024
Rodrigo da Silva <rodrigopds73@gmail.com>
2024-01-13 06:57:16 -0300

    add teams and select teams data fetch

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { env } from "../env";
+import { cookies } from "./cookies";
 
 const axiosClient = axios.create({
   baseURL: env.backendUrl,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${cookies.getJWT()}`,
+  },
 });
 
-axios.interceptors.response.use(
+axiosClient.interceptors.response.use(
   function (response) {
     return response;
   },
   function (error) {
+    console.log("teste");
     if (error.response.status === 401) {
       window.location.href = "/login";
     }
--- frontend/src/pages/Championship.tsx
+++ frontend/src/pages/Championship.tsx
@@ -4,11 +4,52 @@ import { MultiSelect } from "primereact/multiselect";
 import { useState } from "react";
 import PageHeader from "../components/PageHeader";
 import MatchCard from "../components/MatchCard";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { AxiosError } from "axios";
+import axiosClient from "../libs/axios";
+import { SubmitHandler, useForm } from "react-hook-form";
+import InputErrorHelper from "../components/InputErrorHelper";
+import { toast } from "react-toastify";
 
-const teams = [{ name: "Team 1" }, { name: "Team 2" }, { name: "Team 3" }];
+// const teams = [{ name: "Team 1" }, { name: "Team 2" }, { name: "Team 3" }];
+
+type TeamForm = {
+  name: string;
+};
 
 export default function Championship() {
   const [selectedTeams, setSelectedTeams] = useState();
+
+  const { data: teamsData } = useQuery({
+    queryKey: ["teams"],
+    queryFn: () => axiosClient.get("/teams").then((res) => res.data),
+    onError: () => {
+      toast.error("Error fetching teams");
+    },
+  });
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation<any, AxiosError, TeamForm>({
+    mutationFn: (data: TeamForm) => axiosClient.post("/teams", data),
+    onSuccess: () => {
+      toast.success("Team added successfully");
+      queryClient.invalidateQueries({ queryKey: ["teams"] });
+    },
+    onError: (error) => {
+      const errorData = error.response?.data as any;
+      toast.error(errorData.error);
+    },
+  });
+
+  const {
+    register: registerTeam,
+    handleSubmit: handleTeamSubmit,
+    formState: { errors: teamErrors },
+  } = useForm<TeamForm>();
+
+  const onTeamSubmit: SubmitHandler<TeamForm> = (data: TeamForm) => {
+    mutation.mutate(data);
+  };
 
   return (
     <div className="w-full h-full">
@@ -16,18 +57,28 @@ export default function Championship() {
         title="Championship"
         subtitle="Select teams to simulate the championship"
       />
-      <div className="flex flex-column gap-2 mb-5">
-        <label htmlFor="add-team">Add a team</label>
-        <div className="flex gap-2">
-          <InputText id="add-team" />
-          <Button label="Submit" />
+      <form onSubmit={handleTeamSubmit(onTeamSubmit)}>
+        <div className="flex flex-column gap-2 mb-5">
+          <label htmlFor="add-team">Add a team</label>
+          <div className="flex gap-2">
+            <InputText
+              id="add-team"
+              {...registerTeam("name", {
+                required: { value: true, message: "Team name is required" },
+              })}
+            />
+            {teamErrors.name && (
+              <InputErrorHelper error={teamErrors.name.message!} />
+            )}
+            <Button label="Submit" loading={mutation.isLoading} />
+          </div>
         </div>
-      </div>
+      </form>
       <div className="flex flex-column gap-2">
         <label htmlFor="add-team">Select teams</label>
         <div className="flex gap-2">
           <MultiSelect
-            options={teams}
+            options={teamsData?.teams}
             value={selectedTeams}
             onChange={(e) => setSelectedTeams(e.value)}
             placeholder="Teams"
