5d01ec39deba5ad2b9c7041cce11f6588f9f057e
Rodrigo da Silva <rodrigopds73@gmail.com>
2024-01-14 04:11:27 -0300

    update github redirect uri env

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -2,7 +2,8 @@ const env = {
   backendUrl: process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3001",
   githubClientId: process.env.REACT_APP_GITHUB_CLIENT_ID ?? "",
   githubRedirectUri:
-    process.env.REACT_APP_GITHUB_REDIRECT_URI ?? "http://localhost:3000/login",
+    process.env.REACT_APP_GITHUB_REDIRECT_URI ??
+    "http://127.0.0.1:3000/oauth/github",
 };
 
 export { env };
